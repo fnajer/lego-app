e827a9e6d748b5db580482b2c75410bbdd00611f
fnajer <lexactr@gmail.com>
2018-08-08 18:18:26 +0300

    Generally cleanup

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import './styles.css';
 
-import Header from './components/header/Header.jsx';
+import Header from './../Header';
 
 class App extends Component {
   render() {
